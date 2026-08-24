916726db8a837019cb03bf7e52283a3caef2e45f
mohdriyaan <mohammedrayaan1@gmail.com>
2026-08-24 17:07:26 +0000

    feat: add api health endpoint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import express from "express"
 
 const app = express()
-const PORT = 5000 
+const PORT = 5000
+
+app.use(express.json())
 
 app.get("/",(req,res)=>{
   res.send("Relocation Budget API")
+})
+
+app.get("/api/health",(req,res)=>{
+  res.json({
+    status: "ok",
+    message: "Relocation Budget API is running"
+  })
 })
 
 app.listen(PORT,()=>{
