4d87520b4fd945185fbbfaff1c6c93fb20aab981
mohdriyaan <mohammedrayaan1@gmail.com>
2026-08-26 15:40:42 +0000

    feat: return live exchange rate

--- server/controllers/exchangeRateController.js
+++ server/controllers/exchangeRateController.js
@@ -2,21 +2,28 @@ import getExchangeRate from "../services/exchangeRateService.js"
 import validateCurrency from "../utils/validateCurrency.js"
 
 const exchangeRateController = async(req,res) => {
-  const from = validateCurrency(req.params.from)
-  const to = validateCurrency(req.params.to)
+  try{
+    const from = validateCurrency(req.params.from)
+    const to = validateCurrency(req.params.to)
 
-  if(from && to){
-    const rate = await getExchangeRate(from,to)
-    return res.status(200).json({
-      from,
-      to
+    if(from && to){
+      const rate = await getExchangeRate(from,to)
+      return res.status(200).json({
+        from,
+        to,
+        rate
+      })
+    }
+
+    return res.status(400).json({
+      error : "One of the currency is invalid"
+    })
+
+  } catch (error){
+    return res.status(502).json({
+      error : error.message
     })
   }
-
-  return res.status(400).json({
-    error : "One of the currency is invalid"
-  })
-
 }
 
 export default exchangeRateController
