d229fac32fb7df459933cd136959937e2f87d5ec
mohdriyaan <mohammedrayaan1@gmail.com>
2026-09-02 11:45:51 +0000

    fix: render calculator currency validation error

--- client/src/components/CalculatorForm.jsx
+++ client/src/components/CalculatorForm.jsx
@@ -66,7 +66,7 @@ function CalculatorForm({
         </div>
 
         {/* Currency Pair Warning Banner */}
-        {errors.currencyPair && (
+        {errors.destinationCurrency && (
           <div
             role="alert"
             className="p-3.5 rounded-xl bg-amber-500/10 border border-amber-500/30 text-amber-400 text-sm flex items-center gap-2 font-medium"
@@ -74,7 +74,7 @@ function CalculatorForm({
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 shrink-0" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
-            <span>{errors.currencyPair}</span>
+            <span>{errors.destinationCurrency?.message}</span>
           </div>
         )}
 
